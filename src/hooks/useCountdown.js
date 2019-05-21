08287a730b130866ea523f5f053328013bfada19
Parsons, Christian . (S&T-Student) <cpfz9@mst.edu>
2019-05-20 22:24:37 -0500

    Compute countdown time when CountDown is initially rendered

--- src/hooks/useCountdown.js
+++ src/hooks/useCountdown.js
@@ -7,26 +7,30 @@ const initialCountDown = {
   seconds: 0
 };
 
+function computeCountDown(date) {
+  const now = new Date().getTime();
+  const distance = date - now;
+
+  if (distance < 0) {
+    return initialCountDown;
+  } else {
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    return { days, hours, minutes, seconds };
+  }
+}
+
 export default function useCountDown(date = new Date(2019, 10, 16, 15)) {
-  const [countDown, setCountDown] = useState(initialCountDown);
+  const [countDown, setCountDown] = useState(() => computeCountDown(date));
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      const now = new Date().getTime();
-      const distance = date - now;
-
-      if (distance < 0) {
-        setCountDown(initialCountDown);
-      } else {
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        const hours = Math.floor(
-          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-        );
-        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        setCountDown({ days, hours, minutes, seconds });
-      }
+      setCountDown(computeCountDown(date));
     }, 1000);
 
     return () => {
